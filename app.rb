a00d486d3a25200769ebf464a2b9f6d5f8dc5c93
18sasaki <18sasaki@gmail.com>
2014-03-15 21:49:31 +0900

    修正

--- app.rb
+++ app.rb
@@ -21,40 +21,67 @@ before do
 end
 
 get "/" do
-  data = client.favorites(settings.twitter["user"]["screen-name"])
-  @html = ""
-  data.each do |entity|
-    @html += "<img src='#{entity.media[0].media_url}'/><br>" if entity.media[0]
-  end
-
   erb :index
-end
-
-get '/api/get_tweet.json' do
-  limit  = [ (params[:count] || 30).to_i, 150 ].min
-  page   = (params[:page] || 1).to_i
-  offset = (page - 1) * limit
-  max_page  = (150 / limit).ceil
-  next_page = max_page > page ? page + 1 : nil
-  
-  datas = client.favorites(settings.twitter["user"]["screen-name"])
-  
-  content_type :json
-  {:datas => datas, :next => next_page}.to_json
 end
 
 get "/images" do
   count = get_count(params)
   max_id = get_max_id(params)
   result = get_favorited_images(client, count, max_id)
-  
+
   urls = []
   result[:data].each do |e|
     e.media.each do |m|
-      urls << "#{m.media_url}:large" if m
+      urls << "#{m.media_url}" if m
     end
   end
-  
+
+  # urls = case count
+  #        when 50
+  #         p ">>>>>>>>>>>> 50!"
+  #         [
+  #           'http://22448866.up.seesaa.net/image/E8A68BE3819BE381AAE3828CE381AAE38184E38288EFBC81EFBC81.png',
+  #           'http://lohas.nicoseiga.jp//thumb/3109424i',
+  #           'http://ustat.pashaoku.jp/auction/547743/0/L_d542a520-7a94-4cc4-a5fc-483c25fbcb50',
+  #           'http://blog-imgs-36.fc2.com/a/p/g/apg/201302112.jpg',
+  #           'http://lohas.nicoseiga.jp/thumb/3141793i',
+  #           'http://22448866.up.seesaa.net/image/E8A68BE3819BE381AAE3828CE381AAE38184E38288EFBC81EFBC81.png',
+  #           'http://lohas.nicoseiga.jp//thumb/3109424i',
+  #           'http://ustat.pashaoku.jp/auction/547743/0/L_d542a520-7a94-4cc4-a5fc-483c25fbcb50',
+  #           'http://blog-imgs-36.fc2.com/a/p/g/apg/201302112.jpg',
+  #           'http://lohas.nicoseiga.jp/thumb/3141793i',
+  #           'http://22448866.up.seesaa.net/image/E8A68BE3819BE381AAE3828CE381AAE38184E38288EFBC81EFBC81.png',
+  #           'http://lohas.nicoseiga.jp//thumb/3109424i',
+  #           'http://ustat.pashaoku.jp/auction/547743/0/L_d542a520-7a94-4cc4-a5fc-483c25fbcb50',
+  #           'http://blog-imgs-36.fc2.com/a/p/g/apg/201302112.jpg',
+  #           'http://lohas.nicoseiga.jp/thumb/3141793i',
+  #         ]
+  #        when 5
+  #         p ">>>>>>>>>>>> 5!"
+  #         [
+  #           'http://22448866.up.seesaa.net/image/E8A68BE3819BE381AAE3828CE381AAE38184E38288EFBC81EFBC81.png',
+  #           'http://lohas.nicoseiga.jp//thumb/3109424i',
+  #           'http://ustat.pashaoku.jp/auction/547743/0/L_d542a520-7a94-4cc4-a5fc-483c25fbcb50',
+  #           'http://blog-imgs-36.fc2.com/a/p/g/apg/201302112.jpg',
+  #           'http://lohas.nicoseiga.jp/thumb/3141793i'
+  #         ]
+  #        when 4
+  #         p ">>>>>>>>>>>> 4!"
+  #         [
+  #           'http://22448866.up.seesaa.net/image/E8A68BE3819BE381AAE3828CE381AAE38184E38288EFBC81EFBC81.png',
+  #           'http://lohas.nicoseiga.jp//thumb/3109424i',
+  #           'http://ustat.pashaoku.jp/auction/547743/0/L_d542a520-7a94-4cc4-a5fc-483c25fbcb50',
+  #           'http://blog-imgs-36.fc2.com/a/p/g/apg/201302112.jpg'
+  #         ]
+  #        else
+  #         p ">>>>>>>>>>>> else!"
+  #         [
+  #           'http://22448866.up.seesaa.net/image/E8A68BE3819BE381AAE3828CE381AAE38184E38288EFBC81EFBC81.png',
+  #           'http://lohas.nicoseiga.jp//thumb/3109424i',
+  #           'http://blog-imgs-36.fc2.com/a/p/g/apg/201302112.jpg'
+  #         ]
+  #        end
+
   content_type :json
   map = {urls: urls}
   map[:max_id] = result[:max_id] if result[:max_id]
@@ -66,7 +93,7 @@ get "/download" do
   count = get_count(params)
   max_id = get_max_id(params)
   result = get_favorited_images(client, count, max_id)
-  
+
   json = {}
   json[:saved_images] = {}
   json[:existed_images] = {}
@@ -79,7 +106,7 @@ get "/download" do
       p filename
       dirname = File.join(File.dirname(__FILE__), settings.download_dir, e.user.id.to_s)
       FileUtils.mkdir_p(dirname) unless FileTest.exist?(dirname)
-      
+
       filepath = File.join(dirname, filename)
       if FileTest.exist?(filepath)
         json[:existed_images][e.id] = filename
@@ -93,7 +120,7 @@ get "/download" do
       end
     end
   end
-  
+
   content_type :json
   json.to_json
 end
@@ -115,23 +142,23 @@ helpers do
         error = "TooManyRequests";
         break;
       end
-      
+
       break if !data || data.empty?
-      
+
       data.each do |entity|
         result << entity if entity.media?
       end
-      
+
       max_id = data[data.size-1].id - 1
     end
-    
+
     return {data: result, max_id: max_id, error: error.uniq.compact}
   end
-  
+
   def get_count(params)
     [[(params[:count].to_i || 20), 100].min, 1].max
   end
-  
+
   def get_max_id(params)
     (params[:max_id] ? params[:max_id].to_i : nil)
   end
