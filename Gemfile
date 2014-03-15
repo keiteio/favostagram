e4e7a42d9ccef66dc31134d08a318bbecbb92d59
keiteio <mss.ymns@gmail.com>
2014-03-15 17:13:13 +0900

    sample for getting favorited images.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org' 
 
 gem 'sinatra', '1.4.4'
+gem 'sinatra-contrib', '1.4.2'
 gem 'twitter', '5.8.0'
--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
-require "rubygems"
-require "sinatra"
+require 'rubygems'
+require 'Twitter'
+require 'sinatra'
+require 'sinatra/config_file'
+
+config_file 'config/app.yml'
+
+client = nil
+
+before do
+  client = client || Twitter::REST::Client.new do |c|
+    c.consumer_key = settings.twitter["auth"]["api-key"]
+    c.consumer_secret = settings.twitter["auth"]["api-secret"]
+    #c.access_token = settings.twitter["auth"]["acess_token"]
+    #c.access_token_secret = settings.twitter["auth"]["acess_token_secret"]
+  end
+end
 
 get "/" do
-  "Hello world."
+  data = client.favorites(settings.twitter["user"]["screen-name"])
+  
+  html = ""
+  data.each do |entity|
+    html += "<img src='#{entity.media[0].media_url}'/><br>" if entity.media[0]
+  end
+  
+  html
 end
+
 
 
 
